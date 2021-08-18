251bc6af8dd6a5103eba500278bcb6c731fd68ac
michaelcele <michaelcele02@gmail.com>
2021-08-18 12:53:26 -0500

    prototypes functions

--- shell.c
+++ shell.c
@@ -7,12 +7,25 @@ int main(void)
 {
 	int checkprompt;
 	inputdata_t glData = {};
+	char **maintoken;
+	int i = 0;
+	built_in builtin_function[] = {
+			    {"exit", _exit},
+			    {"exec", _exec}, 
+			    {"env", _env}, 
+			    {"setenv", _setenv}, 
+			    {"unsetenv", _unsetenv}, 
+			    {NULL, NULL}};
 
 	/* Checks if its EOF*/
 	while (checkprompt != -1)
 	{
 		checkprompt = promptdisplay(&glData);
-		_strtok(&glData);
+		maintoken=_strtok(&glData);
+		while (i != NULL)
+		{
+			if ()
+		}
 		pid_ppid(&glData);
 		//strtok_path(&glData);
 	}
--- shell_builtins.c
+++ shell_builtins.c
@@ -1,30 +1,40 @@
 #include "shell.h"
-int _exit(inputdata_t *data, char **)
+int _exit(inputdata_t *data, char **tokeninput)
 {
+	(void)data;
+	(void)tokeninput;
 	printf("Entre a exicve");
 
 	return (0);
 }
-int _env(inputdata_t *data, char **)
+int _env(inputdata_t *data, char **tokeninput)
 {
+	(void)data;
+	(void)tokeninput;
 	printf("Entre a exicve");
 
 	return (0);
 }
-int _exec(inputdata_t *data, char **)
+int _exec(inputdata_t *data, char **tokeninput)
 {
+	(void)data;
+	(void)tokeninput;
 	printf("Entre a exicve");
 
 	return (0);
 }
-int _setenv(inputdata_t *data, char **)
+int _setenv(inputdata_t *data, char **tokeninput)
 {
-	printf("Entre a exicve");
+	(void)data;
+	(void)tokeninput;
+	printf("Entre a exicve"tokeninput);
 
 	return (0);
 }
-int _unsetenv(inputdata_t *data, char **)
+int _unsetenv(inputdata_t *data, char **tokeninput)
 {
+	(void)data;
+	(void)tokeninput;
 	printf("Entre a exicve");
 
 	return (0);
